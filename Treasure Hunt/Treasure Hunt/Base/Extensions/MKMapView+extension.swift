296fa545ad5e99c2b4d2303ad0be4e3e9f15e0df
Bahadır Sönmez <bahadir.sonmez@migrosonline.com>
2022-06-12 17:18:33 +0300

    Create mapview extension

---
+++ Treasure Hunt/Treasure Hunt/Base/Extensions/MKMapView+extension.swift
@@ -0,0 +1,21 @@
+//
+//  MKMapView+extension.swift
+//  Treasure Hunt
+//
+//  Created by Bahadır Sönmez on 12.06.2022.
+//
+
+import MapKit
+
+extension MKMapView {
+    func centerToLocation(
+        _ location: CLLocation,
+        regionRadius: CLLocationDistance = 100000
+    ) {
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate,
+            latitudinalMeters: regionRadius,
+            longitudinalMeters: regionRadius)
+        setRegion(coordinateRegion, animated: true)
+    }
+}
