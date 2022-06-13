a98f3d0b1158e838c21d624cd75c8f5bb08e98b5
Bahadır Sönmez <bahadir.sonmez@migrosonline.com>
2022-06-13 23:34:04 +0300

    Add app colors

---
+++ Treasure Hunt/Treasure Hunt/Base/Extensions/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  Treasure Hunt
+//
+//  Created by Bahadır Sönmez on 13.06.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var greenButtonColor: UIColor {
+        return .systemGreen.withAlphaComponent(0.4)
+    }
+    
+    class var redButtonColor: UIColor {
+        return .systemRed.withAlphaComponent(0.4)
+    }
+
+}
