c17d629649d66afdcf83917959531a0ca1e69f0a
Bahadır Sönmez <bahadir.sonmez@migrosonline.com>
2022-06-13 23:24:19 +0300

    Bind map and button

--- Treasure Hunt/Treasure Hunt/Flow/Hunt/HuntViewController.swift
+++ Treasure Hunt/Treasure Hunt/Flow/Hunt/HuntViewController.swift
@@ -6,11 +6,35 @@
 //
 
 import UIKit
+import MapKit
 
 class HuntViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
     }
+    
+    private func setup() {
+        configureButton()
+    }
+    
+    private func configureButton() {
+        startButton.backgroundColor =
+        backButton.backgroundColor = AppManager.color
+        backButton.icornerRadius = 5.0
+        backButton.setTitleColorForAllStates(AppManager.buttonTitleColor)
+        backButton.setTitle(viewModel.buttonTitle, for: .normal)
+        backButton.titleLabel?.font(Fonts.interBoldFont(size: 14.0))
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+    }
+    
     
 }
