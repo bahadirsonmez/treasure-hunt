b24a1dda254c89a7c3d48ef93e17456c62dd3cc8
Bahadır Sönmez <bahadir.sonmez@migrosonline.com>
2022-06-18 11:36:38 +0300

    Change default center radius

--- Treasure Hunt/Treasure Hunt/Base/Extensions/MKMapView+extension.swift
+++ Treasure Hunt/Treasure Hunt/Base/Extensions/MKMapView+extension.swift
@@ -10,7 +10,7 @@ import MapKit
 extension MKMapView {
     func centerToLocation(
         _ location: CLLocation,
-        regionRadius: CLLocationDistance = 100000
+        regionRadius: CLLocationDistance = 1000
     ) {
         let coordinateRegion = MKCoordinateRegion(
             center: location.coordinate,
