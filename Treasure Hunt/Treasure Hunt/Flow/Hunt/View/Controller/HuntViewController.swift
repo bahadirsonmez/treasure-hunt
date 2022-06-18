c2b01ce2e66e57c6766d4c1bc3a620f9772320b4
Bahadır Sönmez <bahadir.sonmez@migrosonline.com>
2022-06-18 11:38:59 +0300

    Generate random annotation in spesific radius

--- Treasure Hunt/Treasure Hunt/Flow/Hunt/View/Controller/HuntViewController.swift
+++ Treasure Hunt/Treasure Hunt/Flow/Hunt/View/Controller/HuntViewController.swift
@@ -20,11 +20,11 @@ class HuntViewController: UIViewController {
     }
     
     private var isStarted: Bool = false
-//    {
-//        didSet {
-//            configureButton()
-//        }
-//    }
+    //    {
+    //        didSet {
+    //            configureButton()
+    //        }
+    //    }
     
     
     override func viewDidLoad() {
@@ -86,7 +86,7 @@ class HuntViewController: UIViewController {
     }
     
     @IBAction private func startButtonTapped(_ sender: UIButton) {
-//        isStarted.toggle()
+        //        isStarted.toggle()
         let contentVC = CurrentHuntDetailViewController()
         let fpc = FloatingPanelController(delegate: contentVC)
         fpc.set(contentViewController: contentVC)
@@ -97,5 +97,50 @@ class HuntViewController: UIViewController {
         fpc.backdropView.dismissalTapGestureRecognizer.numberOfTapsRequired = 3
         self.present(fpc, animated: true, completion: nil)
     }
+    
+    func generateAnnoLoc() {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = generateRandomCoordinates(min: 50, max: 300) //this will be the maximum and minimum distance of the annotation from the current Location (Meters)
+        annotation.title = "Annotation Title"
+        annotation.subtitle = "SubTitle"
+        mapView.addAnnotation(annotation)
+        
+        
+    }
+    
+    func generateRandomCoordinates(min: UInt32, max: UInt32)-> CLLocationCoordinate2D {
+        //Get the Current Location's longitude and latitude
+        let currentLong = currentLocation?.coordinate.longitude ?? 0
+        let currentLat = currentLocation?.coordinate.latitude ?? 0
+        
+        //1 KiloMeter = 0.00900900900901° So, 1 Meter = 0.00900900900901 / 1000
+        let meterCord = 0.00900900900901 / 1000
+        
+        //Generate random Meters between the maximum and minimum Meters
+        let randomMeters = UInt(arc4random_uniform(max) + min)
+        
+        //then Generating Random numbers for different Methods
+        let randomPM = arc4random_uniform(6)
+        
+        //Then we convert the distance in meters to coordinates by Multiplying the number of meters with 1 Meter Coordinate
+        let metersCordN = meterCord * Double(randomMeters)
+        
+        //here we generate the last Coordinates
+        if randomPM == 0 {
+            return CLLocationCoordinate2D(latitude: currentLat + metersCordN, longitude: currentLong + metersCordN)
+        }else if randomPM == 1 {
+            return CLLocationCoordinate2D(latitude: currentLat - metersCordN, longitude: currentLong - metersCordN)
+        }else if randomPM == 2 {
+            return CLLocationCoordinate2D(latitude: currentLat + metersCordN, longitude: currentLong - metersCordN)
+        }else if randomPM == 3 {
+            return CLLocationCoordinate2D(latitude: currentLat - metersCordN, longitude: currentLong + metersCordN)
+        }else if randomPM == 4 {
+            return CLLocationCoordinate2D(latitude: currentLat, longitude: currentLong - metersCordN)
+        }else {
+            return CLLocationCoordinate2D(latitude: currentLat - metersCordN, longitude: currentLong)
+        }
+        
+    }
+    
     
 }
