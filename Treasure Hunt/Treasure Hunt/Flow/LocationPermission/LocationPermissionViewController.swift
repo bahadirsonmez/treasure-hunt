2cb8671c4aad01c6c52cc7b83f4905bc90d7a6b4
Bahadır Sönmez <bahadir.sonmez@migrosonline.com>
2022-06-21 16:06:07 +0300

    Create location permission page

---
+++ Treasure Hunt/Treasure Hunt/Flow/LocationPermission/LocationPermissionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LocationPermissionViewController.swift
+//  Treasure Hunt
+//
+//  Created by Bahadır Sönmez on 21.06.2022.
+//
+
+import UIKit
+
+class LocationPermissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
